e7461f05ff62d0bfa4153726ed97e59228053a73
popovicj211 <jovanpopovic211it@gmail.com>
2023-03-27 02:39:14 +0200

    wip 8

--- WebAPI/Authorization/AuthorizeAttribute.cs
+++ WebAPI/Authorization/AuthorizeAttribute.cs
@@ -17,16 +17,13 @@ namespace WebAPI.Authorization
 
         public void OnAuthorization(AuthorizationFilterContext context)
         {
-            // skip authorization if action is decorated with [AllowAnonymous] attribute
             var allowAnonymous = context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any();
             if (allowAnonymous)
                 return;
 
-            // authorization
             var user = (UserEntity)context.HttpContext.Items["User"];
             if (user == null || (_roles.Any() && !_roles.Contains(user.Role)))
             {
-                // not logged in or role not authorized
                 context.Result = new JsonResult(new { message = "Unauthorized" }) { StatusCode = StatusCodes.Status401Unauthorized };
             }
         }
